3cf38303dd95b007daf1b4ab7cc8498751f0b034
MB <michael.dburke56@gmail.com>
2022-09-27 14:51:00 -0500

    Set compiler version in config & install required npm packages.

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: "0.8.17",
+    version: "0.8.15",
     settings: {
       optimizer: {
         enabled: true,

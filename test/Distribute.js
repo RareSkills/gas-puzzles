c26bc4e8391ca57a79de7b288cd3e4b71ec7bc67
MB <michael.dburke56@gmail.com>
2022-10-03 09:45:52 -0500

    Add testing notes to README.
    Clear old test and set up new test for Distribute contract (not implemented).
    
    Setup the beforeEach deployment.
    
    Remove un-needed file/setup compiler version to match contracts.
    
    Implement payable requires for Distribute func.
    
    Import required packages for HH chain manipulation and big number. Implement business logic test for Distribute.
    
    Implement gas check requirments (need official target gasPrice for function).
    
    Clean up tests.
    
    Implement gas cost check.
    
    Remove deprecated 'revertReason' function/method.
    
    Rework w/ parseEther where possible.
    
    Change the method for measuring gas use.
    
    Log current gas usage while running test.
    
    Conditionally render a 'xxx gas below target' statment.
    
    Resolve typo in gas usage logging.

---
+++ test/Distribute.js
@@ -0,0 +1,91 @@
+const { expect, use } = require('chai');
+const { ethers } = require('hardhat'); 
+const { BigNumber } = ethers;
+const helpers  = require('@nomicfoundation/hardhat-network-helpers');
+
+use(require('chai-as-promised'));
+
+const TARGET_GAS_PRICE = 70_000
+const EIGHT_DAYS = 60 * 60 * 24 * 8; 
+
+
+const logGasUsage = (currentGasUsage) => {
+    const diff = TARGET_GAS_PRICE - currentGasUsage;
+    console.log(`           Current gas use:   ${currentGasUsage}`);
+    console.log(`           The gas target is: ${TARGET_GAS_PRICE}`);
+    if (diff < 0) {
+        console.log(`           You are \x1b[31m${diff * -1}\x1b[0m below the target`);
+    }
+}
+
+describe("Distribute", async function () {
+
+    let instance;
+    let owner;
+    let acct1;
+    let acct2;
+    let acct3;
+    let acct4;
+
+    beforeEach(async() => {
+        [owner, acct1, acct2, acct3, acct4] = await ethers.getSigners();
+        const ContractFactory = await ethers.getContractFactory("OptimizedDistribute");
+        instance = await ContractFactory.deploy([
+            acct1.address,
+            acct2.address,
+            acct3.address,
+            acct4.address,
+        ]);
+
+        await instance.deployed();
+    });
+
+    describe("Payable", function () {
+        it("The functions MUST remain non-payable", async function () {
+            let error;
+            try {
+                await instance.distribute({
+                    value: ethers.utils.parseEther("1.00")
+                });
+            } catch (e) {
+                error = e;
+            }
+
+            expect(error.reason).to.equal('non-payable method cannot override value');
+            expect(error.code).to.equal('UNSUPPORTED_OPERATION');
+            expect(instance.distribute()).to.not.be.rejected;
+        });
+    });
+
+    describe("Gas target", function () {
+        it("The functions MUST meet the expected gas efficiency", async function () {
+
+            await helpers.time.increase(EIGHT_DAYS);
+            await helpers.setBalance(instance.address, ethers.utils.parseEther('1.00'));
+            const gasEstimate = await instance.estimateGas.distribute();
+            
+            logGasUsage(gasEstimate);
+
+            expect(gasEstimate)
+                .to.satisfy(function(val) { return val <= TARGET_GAS_PRICE });
+        });
+    });
+
+    describe("Business logic", function () {
+        it("The functions MUST perform as expected", async function () {
+
+            await expect(instance.distribute()).to.be
+                .rejectedWith('cannot distribute yet');
+
+            await helpers.time.increase(EIGHT_DAYS);
+
+            await helpers.setBalance(instance.address, ethers.utils.parseEther('1.00'));
+            await helpers.setBalance(acct1.address, 0);
+
+            await instance.distribute();
+
+            expect(await ethers.provider.getBalance(acct1.address)).to
+                .equal(new BigNumber.from(ethers.utils.parseEther('0.25')));
+        });
+    });
+});

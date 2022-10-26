a5a581d4caf300cc5d8c457c9c87698b21632e37
MB <michael.dburke56@gmail.com>
2022-10-26 08:38:05 -0500

    Add prettier package and config file.

--- README.md
+++ README.md
@@ -1,36 +1,39 @@
-# RareSkills Gas Puzzles 
+# RareSkills Gas Puzzles
 
-If you want to learn about gas optimization, take the [Udemy gas optimization course](https://www.udemy.com/course/advanced-solidity-understanding-and-optimizing-gas-costs/?referralCode=C4684D6872713525E349)! 
+If you want to learn about gas optimization, take the [Udemy gas optimization course](https://www.udemy.com/course/advanced-solidity-understanding-and-optimizing-gas-costs/?referralCode=C4684D6872713525E349)!
 
 ## Puzzles that are ready for you
-- [x] Distribute (hard)
-- [x] Array Sum (easy)
-- [ ] Escrow
-- [ ] EscrowV2
-- [ ] Mint
-- [ ] Presale
-- [x] Require (easy)
-- [ ] Staking
+
+-   [x] Distribute (hard)
+-   [x] Array Sum (easy)
+-   [ ] Escrow
+-   [ ] EscrowV2
+-   [ ] Mint
+-   [ ] Presale
+-   [x] Require (easy)
+-   [ ] Staking
 
 ## Contributors
+
 DO NOT COMMIT SOLUTIONS, BE SURE TO PUT ANSWERS IN `contracts/contracts_optimized` to ensure they fall into the `.gitignore`
 
 ## Players
+
 Your goal is to optimize the contracts such that they reach the target efficiency.
 
 Rules
-- you may not change the optimizer level
-- you may not change the solidity version
-- you may refactor functionality as long as you don't break the business logic
-- you may make reasonable assumptions about what variable sizes are necessary to get things done
-- you may remove unnecessary or redunant logic (some have been intentionally added)
-- because making functions `payable` is a controversial optimization, you do not need to make functions `payable` to reach the gas target unless the function needs to be payable to fulfill its business logic
 
+-   you may not change the optimizer level
+-   you may not change the solidity version
+-   you may refactor functionality as long as you don't break the business logic
+-   you may make reasonable assumptions about what variable sizes are necessary to get things done
+-   you may remove unnecessary or redunant logic (some have been intentionally added)
+-   because making functions `payable` is a controversial optimization, you do not need to make functions `payable` to reach the gas target unless the function needs to be payable to fulfill its business logic
 
 ## Testing
 
 As mentioned above, optimized contracts should be created in their own sub-folder
-to prevent commiting them publically. 
+to prevent commiting them publicly.
 
 The file structure should look similar to this:
 
@@ -52,7 +55,7 @@ The file structure should look similar to this:
     ...
 ```
 
-Within the contracts that are optimized be sure to follow the following naming 
+Within the contracts that are optimized be sure to follow the following naming
 convention to ensure tests run smoothly:
 
 ```
@@ -65,6 +68,8 @@ contract OptimizedDistribute {
 ...
 ```
 
-All tests can be run with: `npx hardhat test`
-
-Specific tests can be run with: `npx hardhat test test/ArraySum`
+```
+npx hardhat test
+npx hardhat test test/ArraySum
+npx prettier --write *
+```
--- test/ArraySum.js
+++ test/ArraySum.js
@@ -1,9 +1,9 @@
-const { expect, use } = require("chai");
-const { ethers } = require("hardhat");
+const { expect, use } = require('chai');
+const { ethers } = require('hardhat');
 const { BigNumber } = ethers;
-const helpers = require("@nomicfoundation/hardhat-network-helpers");
+const helpers = require('@nomicfoundation/hardhat-network-helpers');
 
-use(require("chai-as-promised"));
+use(require('chai-as-promised'));
 
 const TARGET_GAS_PRICE = 23_396;
 const EIGHT_DAYS = 60 * 60 * 24 * 8;
@@ -19,39 +19,39 @@ const logGasUsage = (currentGasUsage) => {
     }
 };
 
-describe("ArraySum", async function () {
+describe('ArraySum', async function () {
     let instance;
 
     beforeEach(async () => {
         const ContractFactory = await ethers.getContractFactory(
-            "OptimizedArraySum"
+            'OptimizedArraySum'
         );
         instance = await ContractFactory.deploy();
 
         await instance.deployed();
     });
 
-    describe("Payable", function () {
-        it("The functions MUST remain non-payable", async function () {
+    describe('Payable', function () {
+        it('The functions MUST remain non-payable', async function () {
             let error;
             try {
                 await instance.getArraySum({
-                    value: ethers.utils.parseEther("1.00"),
+                    value: ethers.utils.parseEther('1.00'),
                 });
             } catch (e) {
                 error = e;
             }
 
             expect(error.reason).to.equal(
-                "non-payable method cannot override value"
+                'non-payable method cannot override value'
             );
-            expect(error.code).to.equal("UNSUPPORTED_OPERATION");
+            expect(error.code).to.equal('UNSUPPORTED_OPERATION');
             expect(instance.getArraySum()).to.not.be.rejected;
         });
     });
 
-    describe("Gas target", function () {
-        it("The functions MUST meet the expected gas efficiency", async function () {
+    describe('Gas target', function () {
+        it('The functions MUST meet the expected gas efficiency', async function () {
             const gasEstimate = await instance.estimateGas.getArraySum();
 
             logGasUsage(gasEstimate);
@@ -62,8 +62,8 @@ describe("ArraySum", async function () {
         });
     });
 
-    describe("Business logic", function () {
-        it("The functions MUST perform as expected", async function () {
+    describe('Business logic', function () {
+        it('The functions MUST perform as expected', async function () {
             await instance.setArray([1, 2, 3, 4, 5, 6, 7, 8, 9]);
             expect(await instance.getArraySum()).to.equal(45);
 
@@ -73,7 +73,7 @@ describe("ArraySum", async function () {
             expect(await instance.getArraySum()).to.equal(4500);
         });
 
-        it("should not overflow", async function () {
+        it('should not overflow', async function () {
             await instance.setArray([2n ** 256n - 1n, 4n]);
             await expect(instance.getArraySum()).to.be.reverted;
         });

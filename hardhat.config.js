aff4962b794b2b551f55660f940d226110b178af
MB <michael.dburke56@gmail.com>
2022-10-26 08:38:05 -0500

    Add prettier package and config file.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: {
-    version: "0.8.15",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 1000000
-      }
-    }
-  },
-  mocha: {
-    timeout: 40000
-  }
+    solidity: {
+        version: '0.8.15',
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 1000000,
+            },
+        },
+    },
+    mocha: {
+        timeout: 40000,
+    },
 };

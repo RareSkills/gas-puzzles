8f4ce172514a4cde380ec29c4399af9d9ca41629
Jeffrey Scholz <scholz.jeffrey@gmail.com>
2022-10-27 11:11:24 +0700

    Update Mint150.sol
    clarify openzeppelin cannot be modified

--- contracts/Mint150.sol
+++ contracts/Mint150.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.15;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-// You may not modify this contract
+// You may not modify this contract or the openzeppelin contracts
 contract NotRareToken is ERC721 {
     mapping(address => bool) private alreadyMinted;
 

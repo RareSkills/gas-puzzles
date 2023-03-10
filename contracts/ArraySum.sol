604977fcb6b863e3680873681490eb33dd682c8b
Jeffrey Scholz <jeffreyscholz@Jeffreys-MacBook-Pro.local>
2023-03-10 22:01:57 +0700

    clarify scope of optimization

--- contracts/ArraySum.sol
+++ contracts/ArraySum.sol
@@ -2,13 +2,16 @@
 pragma solidity 0.8.15;
 
 contract ArraySum {
+    // Do not modify this
     uint256[] array;
 
+    // Do not modify this
     function setArray(uint256[] memory _array) external {
-        require(_array.length <= 10, "too long");
+        require(_array.length <= 10, 'too long');
         array = _array;
     }
 
+    // optimize this function
     function getArraySum() external view returns (uint256) {
         uint256 sum;
         for (uint256 i = 0; i < array.length; i++) {
--- contracts/Require.sol
+++ contracts/Require.sol
@@ -2,14 +2,16 @@
 pragma solidity 0.8.15;
 
 contract Require {
+    // Do not modify these variables
     uint256 constant COOLDOWN = 1 minutes;
     uint256 lastPurchaseTime;
 
+    // Optimize this function
     function purchaseToken() external payable {
         require(
             msg.value == 0.1 ether &&
                 block.timestamp > lastPurchaseTime + COOLDOWN,
-            "cannot purchase"
+            'cannot purchase'
         );
         lastPurchaseTime = block.timestamp;
         // mint the user a token
